e07634090ca31dade16acab435197141ef638c6d
alessandra150601-commits <alessandra150601@edu.unipar.br>
2025-08-18 22:22:55 -0300

    Add files via upload

---
+++ codigos_prog/exemplo_01.java
@@ -0,0 +1,30 @@
+class exemplo_01 {
+
+    public static void main ( String [] args){
+
+          System.out.println("Olá Mundo! \n\nEsse é meu primeiro programa em Java.");
+
+          int idade;
+          idade = 15; 
+         
+          System.out.println("idade: " + idade);
+          
+          //declarar uma variável do tipo String chamada nome:
+          String nome;
+          nome = "Alecks";
+          
+          System.out.println("nome: " + nome );
+
+         //declrarar uma variável do tipo boolean chamada status_aluno:
+          boolean status_aluno; 
+          status_aluno = true; //true = verdadeiro
+          System.out.println(status_aluno);
+          status_aluno = false; //true = falso
+         
+         //declarar uma variável do tipo char chamada periodo:
+          char periodo;
+          periodo = '2';
+          System.out.println(periodo);
+    }
+
+}
---
+++ codigos_prog/exemplo_operadores.java
@@ -0,0 +1,44 @@
+class exemplo_operadores{
+    
+    public static void main ( String [] args) {
+      System.out.println("resultados das operacoes entre A e B:");
+      int A;
+      A = 7;
+      System.out.println(A);
+      
+      int B;
+      B = 10;
+      System.out.println(B);
+      
+      int adicao;
+      
+      adicao = A + B;
+      System.out.println(adicao);
+      
+      int subtracao;
+      
+      subtracao = A + B;
+      System.out.println(subtracao);
+
+      int multiplicacao;
+
+      multiplicacao = A + B; 
+      System.out.println(multiplicacao);
+
+      double divisao; 
+
+      divisao = A / B;
+      System.out.println(divisao);
+
+      double resto_divisao;
+      
+      resto_divisao = A % B;
+      System.out.println(resto_divisao);
+     
+     
+     }
+
+
+
+
+}
