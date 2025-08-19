011645b924af3815004317dfff344c885a8dce6f
alessandra150601-commits <alessandra150601@edu.unipar.br>
2025-08-18 22:36:45 -0300

    Add files via upload

---
+++ exemplo_operadores.java
@@ -0,0 +1,44 @@
+class exemplo_operadores{
+    
+    public static void main ( String [] args) {
+      
+      int A;
+      A = 7;
+      System.out.println("A: " + A);
+      
+      int B;
+      B = 10;
+      System.out.println("B: " + B);
+      
+      int adicao;
+      
+      adicao = A + B;
+      System.out.println("adicao: " + adicao);
+      
+      int subtracao;
+      
+      subtracao = A + B;
+      System.out.println("subtracao: " + subtracao);
+
+      int multiplicacao;
+
+      multiplicacao = A + B; 
+      System.out.println("multiplicacao: " + multiplicacao);
+
+      double divisao; 
+
+      divisao = A / B;
+      System.out.println("divisao: " + divisao);
+
+      double resto_divisao;
+      
+      resto_divisao = A % B;
+      System.out.println("resto_divisao: " + resto_divisao);
+     
+     
+     }
+
+
+
+
+}
